376ff9aec9f92917f195a58ea7fd6b7b4b419774
Wyndace (Васильев Александр) <wyndace@hotmail.com>
2021-07-17 22:55:22 +0500

    added geting time and links functions

--- parser.py
+++ parser.py
@@ -89,12 +89,57 @@ def get_thumbnail(game):
         return None
 
 
+def get_begin_time(game):
+    time_str = None
+    try:
+        if game["promotions"]["promotionalOffers"] != []:
+            time_str = game["promotions"]["promotionalOffers"][0]["promotionalOffers"][0]["startDate"]
+        elif game["promotions"]["promotionalOffers"] == []:
+            time_str = game["promotions"]["upcomingPromotionalOffers"][0]["promotionalOffers"][0]["startDate"]
+        time = None
+        if time_str != None:
+            time = datetime.strptime(
+                str(time_str), "%Y-%m-%dT%H:%M:%S.000Z").timestamp()
+        return time
+    except Exception as ex:
+        print(ex)
+        return None
+
+
+def get_end_time(game):
+    time_str = None
+    try:
+        if game["promotions"]["promotionalOffers"] != []:
+            time_str = game["promotions"]["promotionalOffers"][0]["promotionalOffers"][0]["endDate"]
+        elif game["promotions"]["promotionalOffers"] == []:
+            time_str = game["promotions"]["upcomingPromotionalOffers"][0]["promotionalOffers"][0]["endDate"]
+        time = None
+        if time_str != None:
+            time = datetime.strptime(
+                str(time_str), "%Y-%m-%dT%H:%M:%S.000Z").timestamp()
+        return time
+    except Exception as ex:
+        print(ex)
+        return None
+
+
+def get_link(game):
+    try:
+        return f'https://www.epicgames.com/p/{game["catalogNs"]["mappings"][0]["pageSlug"]}'
+    except Exception as ex:
+        print(ex)
+        return None
+
+
 def get_min_game_info(game):
     new_info = {}
     new_info["title"] = get_title(game)
-    new_info["price"] = get_price(game)
     new_info["company_seller"] = get_company_seller(game)
+    new_info["price"] = get_price(game)
+    new_info["link"] = get_link(game)
     new_info["thumbnail"] = get_thumbnail(game)
+    new_info["beginTime"] = get_begin_time(game)
+    new_info["endTime"] = get_end_time(game)
     new_info["status"] = game['status']
     return new_info
 
@@ -108,6 +153,7 @@ def build_min_games_list(country):
     filedata['allGames'] = []
     filedata['activeGames'] = []
     filedata['upcomingGames'] = []
+    filedata['activeUpcomingGames'] = []
     filedata['endedGames'] = []
     for game in games:
         new_info = get_min_game_info(game)
@@ -115,9 +161,11 @@ def build_min_games_list(country):
     for game in active_games:
         new_info = get_min_game_info(game)
         filedata['activeGames'].append(new_info)
+        filedata['activeUpcomingGames'].append(new_info)
     for game in upcoming_games:
         new_info = get_min_game_info(game)
         filedata['upcomingGames'].append(new_info)
+        filedata['activeUpcomingGames'].append(new_info)
     for game in ended_games:
         new_info = get_min_game_info(game)
         filedata['endedGames'].append(new_info)
