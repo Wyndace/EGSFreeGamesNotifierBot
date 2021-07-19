78887893d3d702018f424275e8c062e8ae1c86aa
Wyndace (Васильев Александр) <wyndace@hotmail.com>
2021-07-20 02:56:37 +0500

    added wiki parser for getting the all ISO Codes

--- wiki.py
+++ wiki.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 from requests import get
 from bs4 import BeautifulSoup
 import pprint
 
 
-def get_country_code():
-    url = f'https://wiki2.org/en/ISO_3166-2'
+def get_country_code(lang):
+    url = f'https://wiki2.org/{lang}/ISO_3166-2'
     params = {
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0"
     }
@@ -20,10 +19,10 @@ def get_country_code():
             str(BeautifulSoup(str(line), 'lxml').findAll('a')[0]), 'lxml').text
         country = BeautifulSoup(
             str(BeautifulSoup(str(line), 'lxml').findAll('a')[1]), 'lxml').text
-        code_country[code] = str(country)
+        code_country[code] = str(country).replace('\u2005', ' ')
         codes_with_countries.append(code_country)
     return codes_with_countries
 
 
 if __name__ == "__main__":
-    pprint.pprint(get_country_code('ru'))
+    pprint.pprint(get_country_code())
